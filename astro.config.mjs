55cf015af28cadc2e28d4bc298969df968eba53d
Ere Männistö <connect@eremannisto.com>
2025-02-09 11:42:01 +0200

    Fix: Add missing `enabled` flag to `i18n` config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // Custom application configuration
 export const app = {
   i18n: {
+    enabled  : true,
     fallback : 'en',
     locales  : {
       en : { name: 'English', endonym: 'English', direction: 'ltr' },
