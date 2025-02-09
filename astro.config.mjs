03b11dfbf4ffa78fa7294ec656489371514d2320
Ere Männistö <connect@eremannisto.com>
2025-02-09 14:11:59 +0200

    Chore: Add default `errors` configuration for the template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // Custom application configuration
 export const app = {
+  errors: {
+    enabled   : true,
+    supported : [
+      400, // Bad Request
+      401, // Unauthorized
+      403, // Forbidden
+      404, // Not Found
+      408, // Request Timeout
+      418, // I'm a Teapot
+      429, // Too Many Requests
+      500, // Internal Server Error
+      503  // Service Unavailable
+    ],
+    fallback: {
+      clientError : 404,  // Not Found
+      serverError : 500   // Internal Server Error
+    }
+  },
+
   i18n: {
     enabled  : true,
     fallback : 'en',
